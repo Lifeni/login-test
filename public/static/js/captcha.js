2a39883e6c088906c2c04ac497a9b381c24ed6b8
Lifeni <liangfengning@foxmail.com>
2020-05-10 16:44:04 +0800

    添加密码提示，优化界面

--- public/static/js/captcha.js
+++ public/static/js/captcha.js
@@ -1,6 +1,27 @@
 'use strict';
 
-const num1 = Math.round(Math.random() * 20);
-const num2 = Math.round(Math.random() * 20);
-const answer = num1 + num2;
-document.querySelector('#captcha-text').innerText = `${num1} + ${num2} = ?`;
+document.addEventListener('DOMContentLoaded', () => {
+    getCaptcha();
+})
+
+const captchaText = document.querySelector('#captcha-text');
+const captcha = document.querySelector('#captcha');
+
+captchaText.addEventListener('click', getCaptcha);
+
+function getCaptcha() {
+    const num1 = Math.round(Math.random() * 20);
+    const num2 = Math.round(Math.random() * 20);
+    const answer = num1 + num2;
+
+    captchaText.innerText = `${num1} + ${num2} = ?`;
+
+    captcha.addEventListener('input', (e) => {
+        console.log(e);
+        if (e.target.value === answer.toString()) {
+            captcha.disabled = true;
+            captchaText.innerText = 'Verified'
+            captchaText.removeEventListener('click', getCaptcha);
+        }
+    })
+}
